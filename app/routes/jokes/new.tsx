d8cb01a258a738dfa0387a22f1f8739e20e2dfd2
Abdo Amin <abdo.amin9991@gmail.com>
2022-04-23 20:20:35 +0200

    add form validation

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,33 +1,71 @@
 import type { ActionFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name: string | undefined;
+    content: string | undefined;
+  };
+  fields?: {
+    name: string;
+    content: string;
+  };
+};
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const [name, content] = [formData.get("name"), formData.get("content")];
   if (typeof name !== "string" || typeof content !== "string") {
-    throw new Error(`Form not submitted correctly.`);
+    return badRequest({ formError: "Invalid form data" });
   }
-  const errors = {
+  const fieldErrors = {
     name: name.length > 3 ? undefined : "Name must be at least 3 characters.",
-    content: content.length > 10 ? undefined : "Content must be at least 10 characters.",
+    content:
+      content.length > 10
+        ? undefined
+        : "Content must be at least 10 characters.",
+  };
+  const fields = { name, content };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
   }
+
   const joke = await db.joke.create({
-    data: { name, content },
+    data: fields,
   });
 
   return redirect(`/jokes/${joke.id}`);
 };
 
 export default () => {
+  const actionData = useActionData();
   return (
     <div>
       <p>Add your own hilarious joke</p>
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name:{" "}
+            <input
+              type="text"
+              defaultValue={actionData?.fields?.name}
+              name="name"
+              aria-invalid={Boolean(actionData?.fieldErrors?.name) || undefined}
+              aria-errormessage={
+                actionData?.fieldErrors?.name ? "name-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.name ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
         </div>
         <div>
           <label>
