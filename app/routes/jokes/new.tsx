ffc9f3fe10c76fa052ab41f472c1de47acb27f76
Abdo Amin <abdo.amin9991@gmail.com>
2022-04-23 20:02:08 +0200

    add new a joke.

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,24 @@
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const [name, content] = [formData.get("name"), formData.get("content")];
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error(`Form not submitted correctly.`);
+  }
+  const errors = {
+    name: name.length > 3 ? undefined : "Name must be at least 3 characters.",
+    content: content.length > 10 ? undefined : "Content must be at least 10 characters.",
+  }
+  const joke = await db.joke.create({
+    data: { name, content },
+  });
+
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default () => {
   return (
     <div>
